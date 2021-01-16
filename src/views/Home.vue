e30c611c99bfd2be707c21f309647443fecce43f
Pijaru <harsono.kltn@gmail.com>
2021-01-16 23:16:11 +0700

    bagian 3 video 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,10 @@
   <div class="home">
       <HeaderShayna />
       <SlideHero />
+      <ProductSlide />
+      <InstaShayna />
+      <PartnerLogo />
+      <FooterShayna />
 
 
   </div>
@@ -12,12 +16,20 @@
 //import HelloWorld from '@/components/HelloWorld.vue'
 import HeaderShayna from '@/components/HeaderShayna.vue'
 import SlideHero from '@/components/SlideHero.vue'
+import ProductSlide from '@/components/ProductSlide.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerLogo from '@/components/PartnerLogo.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
 //import carousel from 'vue-owl-carousel'
 export default {
   name: 'Home',
   components: {
     HeaderShayna,
     SlideHero,
+    ProductSlide,
+    InstaShayna,
+    PartnerLogo,
+    FooterShayna
     //carousel
   }
 }
