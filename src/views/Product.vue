7bb80551e15798a1dfb645d767e096eb5ee5e3ad
Pijaru <harsono.kltn@gmail.com>
2021-01-20 19:48:33 +0700

    video 3 part 6

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,12 +2,12 @@
   <div class="product">
       <HeaderShayna />
             <!-- Breadcrumb Section Begin -->
-    <div class="breacrumb-section">
+    <div class="breacrumb-section text-left" >
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
                     <div class="breadcrumb-text product-more">
-                        <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+                        <router-link to="/"><i class="fa fa-home"></i> Home</router-link> 
                         <span>Detail</span>
                     </div>
                 </div>
@@ -24,35 +24,35 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="product-pic-zoom">
-                                <img class="product-big-img" src="img/mickey1.jpg" alt="" />
+                                <img class="product-big-img" :src="gambar_default" alt="" />
                             </div>
                             <div class="product-thumbs">
                                 <carousel class="product-thumbs-track ps-slider" :items="3" :nav="false" :dots="false" :autoplay="true">
-                                    <div class="pt active" data-imgbigurl="img/mickey1.jpg">
+                                    <div class="pt active" @click="changeImage(thumbs[0])" :class="thumbs[0] == gambar_default ? 'active' : '' " >
                                         <img src="img/mickey1.jpg" alt="" />
                                     </div>
 
-                                    <div class="pt" data-imgbigurl="img/mickey2.jpg">
+                                    <div class="pt" @click="changeImage(thumbs[1])" :class="thumbs[1] == gambar_default ? 'active' : '' " >
                                         <img src="img/mickey2.jpg" alt="" />
                                     </div>
 
-                                    <div class="pt" data-imgbigurl="img/mickey3.jpg">
+                                    <div class="pt" @click="changeImage(thumbs[2])" :class="thumbs[2] == gambar_default ? 'active' : '' " >
                                         <img src="img/mickey3.jpg" alt="" />
                                     </div>
 
-                                    <div class="pt" data-imgbigurl="img/mickey4.jpg">
+                                    <div class="pt" @click="changeImage(thumbs[3])" :class="thumbs[3] == gambar_default ? 'active' : '' " >
                                         <img src="img/mickey4.jpg" alt="" />
                                     </div>
                                 </carousel>
                             </div>
                         </div>
                         <div class="col-lg-6">
-                            <div class="product-details">
-                                <div class="pd-title">
+                            <div class="product-details text-left">
+                                <div class="pd-title text-left">
                                     <span>oranges</span>
                                     <h3>Pure Pineapple</h3>
                                 </div>
-                                <div class="pd-desc">
+                                <div class="pd-desc text-left">
                                     <p>
                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
                                     </p>
@@ -93,6 +93,22 @@ export default {
     HeaderShayna,
     FooterShayna,
     carousel
+  },
+  data() {
+      return {
+          gambar_default :"img/mickey1.jpg",
+          thumbs: [
+              "img/mickey1.jpg",
+              "img/mickey2.jpg",
+              "img/mickey3.jpg",
+              "img/mickey4.jpg"
+          ] 
+      }
+  },
+  methods: {
+      changeImage(urlImage) {
+          this.gambar_default = urlImage;
+      }
   }
 }
 </script>
